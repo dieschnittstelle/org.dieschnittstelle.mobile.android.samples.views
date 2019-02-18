0cd933e5a6c2456bf276fe06f265dfdd53384d31
jk <joern.kreutel@beuth-hochschule.de>
2019-02-18 10:29:20 +0100

    unify settings for s19

--- app/src/main/java/org/dieschnittstelle/mobile/android/views/widgets/DatePickerActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/views/widgets/DatePickerActivity.java
@@ -34,7 +34,7 @@ public class DatePickerActivity extends Activity {
 				dateAsText.setText(dt.toString());
 				
 				// ueberpruefe das Datum
-				if (year < 2015) {
+				if (year < 2019) {
 					startActivity(new Intent(DatePickerActivity.this,ListViewActivity.class));
 				}
 			}
