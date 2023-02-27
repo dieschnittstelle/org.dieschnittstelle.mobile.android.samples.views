2db5985ad46d77851e7163a1a7e97f5f96cae702
jk <joern.kreutel@beuth-hochschule.de>
2023-02-27 23:23:40 +0100

    change reference date for date picker

--- app/src/main/java/org/dieschnittstelle/mobile/android/views/widgets/DatePickerActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/views/widgets/DatePickerActivity.java
@@ -34,7 +34,7 @@ public class DatePickerActivity extends Activity {
 				dateAsText.setText(dt.toString());
 				
 				// ueberpruefe das Datum
-				if (year < 2022) {
+				if (year < 2023) {
 					startActivity(new Intent(DatePickerActivity.this,ListViewActivity.class));
 				}
 			}
